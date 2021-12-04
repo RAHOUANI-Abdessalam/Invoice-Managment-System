6f98b4b045033f999aa449b4c646a700b5a96238
RAHOUANI-Abdessalam <abdelsalamrahouani@gmail.com>
2021-12-04 22:03:55 +0100

    i add the Ajouter Client Ui we just nedd to keep it unresizable

---
+++ src/AjouterClientController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author abdel
+ */
+public class AjouterClientController implements Initializable {
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
