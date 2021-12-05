34e1e845e961118f2b257a079b72d22dcf649435
RAHOUANI-Abdessalam <abdelsalamrahouani@gmail.com>
2021-12-05 16:46:47 +0100

    Ajouter Client Ui with css font family inn AjouterClient2.fxml

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
       
-        Parent root = FXMLLoader.load(getClass().getResource("main page.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("AjouterClient2.fxml"));
         
         Scene scene = new Scene(root);
         
