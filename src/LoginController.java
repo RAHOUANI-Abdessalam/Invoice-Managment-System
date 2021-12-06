c753989670b8f7a18516d1713fd7614c731963d9
RAHOUANI-Abdessalam <abdelsalamrahouani@gmail.com>
2021-12-06 15:15:27 +0100

    code the login Page with sql

--- src/LoginController.java
+++ src/LoginController.java
@@ -1,17 +1,29 @@
 
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
+import project.ConnectionProvider;
 
 
 public class LoginController implements Initializable {
@@ -39,7 +51,7 @@ public class LoginController implements Initializable {
     }    
     
     @FXML
-    private void btnEntrer(){
+    private void btnEntrer() throws IOException{
         if(usernameField.getText().isEmpty()){
             //errorMsg.show("Nom D'utilisateu est vide", 1500);
 //            Alert alert = new Alert(AlertType.INFORMATION);
@@ -64,21 +76,62 @@ public class LoginController implements Initializable {
             return;
         }
         
-        System.out.println("Username: "+usernameField.getText());       
-        System.out.println("Password: "+motdepasseField.getText());
+         try {
+                    Connection con=ConnectionProvider.getCon();
+                    Statement st = con.createStatement();
+                    ResultSet rs= st.executeQuery("select *from user where username='"+usernameField.getText()+"' and password='"+motdepasseField.getText()+"'");
+                    if(rs.next()){
+                        
+                       //close the Login Frame "Stage"
+                        con.close();
+                        Stage stage = (Stage) motdepasseField.getScene().getWindow();
+                        stage.close();
+
+                        //Creat a new Satge to Show the New frame "the Deshboard"
+                        Stage primaryStage =new Stage();
+                        FXMLLoader loader =new FXMLLoader();
+                        Parent root = loader.load(getClass().getResource("Deshboard.fxml"));        
+                        //Pane root = loader.load(getClass().getResource("Deshboard.fxml"));
+                        Scene scene = new Scene(root);
+                        primaryStage.setTitle("Se Connecter");
+                        primaryStage.setScene(scene);
+                        primaryStage.setMinHeight(720);
+                        primaryStage.setMinWidth(1280);
+                        primaryStage.show();
+                    }else{
+                        String toastMsg = "                   Désolé \n Vous avez mal saisi les données";
+                        int toastMsgTime = 2500; //2.5 seconds
+                        int fadeInTime = 500; //0.5 seconds
+                        int fadeOutTime= 500; //0.5 seconds
+                        Toast.makeText((Stage) motdepasseField.getScene().getWindow(), toastMsg, toastMsgTime, fadeInTime, fadeOutTime);
+                    }
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null,""+e.toString());
+                }
         
-        String toastMsg = "Bienvenu "+usernameField.getText();
-        int toastMsgTime = 3500; //3.5 seconds
-        int fadeInTime = 500; //0.5 seconds
-        int fadeOutTime= 500; //0.5 seconds
-        Toast.makeText((Stage) motdepasseField.getScene().getWindow(), toastMsg, toastMsgTime, fadeInTime, fadeOutTime);
     }
     
     @FXML
     private void btnClose(){
+//         to close just one stage and other still running
 //         Stage stage = (Stage) motdepasseField.getScene().getWindow();
 //         stage.close();
-         Platform.exit();
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Fermer");
+            //alert.setHeaderText("SVP, Vérifier");
+            alert.setHeaderText(null);
+            alert.setContentText("Voulez vous Vraiment fermer");
+            
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK){
+                // ... user chose OK
+                //close the entier app
+                Platform.exit();
+            } else {
+                // ... user chose CANCEL or closed the dialog
+                alert.close();
+            }
+         
     }
     
         @FXML
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,9 @@
 
 import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
       
-        Parent root = FXMLLoader.load(getClass().getResource("Deshboard.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         
         Scene scene = new Scene(root);
         
