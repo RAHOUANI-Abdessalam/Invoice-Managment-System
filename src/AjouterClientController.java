8bd96a0b6dff54ee12066b9c75a093c5f7999687
RAHOUANI-Abdessalam <abdelsalamrahouani@gmail.com>
2021-12-06 21:47:56 +0100

    backend of ajouter client and modifier client

--- src/AjouterClientController.java
+++ src/AjouterClientController.java
@@ -4,9 +4,24 @@
  * and open the template in the editor.
  */
 
+import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javax.swing.JOptionPane;
+import project.ConnectionProvider;
 
 /**
  * FXML Controller class
@@ -15,10 +30,96 @@ import javafx.fxml.Initializable;
  */
 public class AjouterClientController implements Initializable {
 
+    @FXML
+    private Text numClientText;
+    @FXML
+    private JFXTextField raisonScocialField;
+    @FXML
+    private JFXTextField adresseField;
+    @FXML
+    private JFXTextField matriculeFiscalField;
+    @FXML
+    private JFXTextField n_articleField;
+    @FXML
+    private JFXTextField registrDeCommrcField;
+    int id;
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+                try {
+                    Connection con = ConnectionProvider.getCon();
+                    Statement st = con.createStatement();
+                    ResultSet rs = st.executeQuery("select *from client ORDER BY numeroClient DESC LIMIT 1");
+                    if(rs.next()){
+                        id = rs.getInt("numeroClient");
+                        id=id+1;
+                        String str = String.valueOf(id);
+                        numClientText.setText(str);
+                    }else
+                        numClientText.setText("1");
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null,""+e.toString());
+                }
     }    
     
+    @FXML
+    private void btnClose(){
+         //to close just one stage and other still running
+         Stage stage = (Stage) raisonScocialField.getScene().getWindow();
+         stage.close();    
+        // Platform.exit();
+    }
+    
+    @FXML
+    private void btnAjouterClient(){
+                if(raisonScocialField.getText().isEmpty() || adresseField.getText().isEmpty() || matriculeFiscalField.getText().isEmpty() || n_articleField.getText().isEmpty() || registrDeCommrcField.getText().isEmpty() ){
+                    Toast.makeText((Stage) raisonScocialField.getScene().getWindow(), "Veuilley saisir tout les champs SVP", 1500, 500, 500);
+                    return;
+                }
+                try {
+                    Connection con = ConnectionProvider.getCon();
+                    Statement st = con.createStatement();
+                    st.executeUpdate("insert into client values('0','"+raisonScocialField.getText()+"','"+adresseField.getText()+"','"+matriculeFiscalField.getText()+"','"+n_articleField.getText()+"','"+registrDeCommrcField.getText()+"')");
+                    //Toast.makeText((Stage) raisonScocialField.getScene().getWindow(), "Clien Numéro "+id+" a été Ajouté avec succès", 2000, 500, 500);
+                   
+                    Stage stage = (Stage) raisonScocialField.getScene().getWindow();
+                    stage.close();  
+                    
+                    //Creat a new Satge to Show the New frame "the Deshboard"
+                    Stage primaryStage =new Stage();
+                    FXMLLoader loader =new FXMLLoader();
+                    //Parent root = loader.load(getClass().getResource("Deshboard.fxml"));        
+                    Pane root = loader.load(getClass().getResource("AjouterClient.fxml"));
+                    Scene scene = new Scene(root);
+                    primaryStage.setTitle("Ajouter Client");
+                    primaryStage.setScene(scene);
+                    primaryStage.setResizable(false);
+                    primaryStage.show();
+                    
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null,""+e.toString());
+                    System.out.println("Error in connection"+e.toString());
+        }
+    }
+    
+    @FXML
+    private void btnReinitialiser() throws IOException{
+        
+                    Stage stage = (Stage) raisonScocialField.getScene().getWindow();
+                    stage.close();  
+                    
+                    //Creat a new Satge to Show the New frame "the Deshboard"
+                    Stage primaryStage =new Stage();
+                    FXMLLoader loader =new FXMLLoader();
+                    //Parent root = loader.load(getClass().getResource("Deshboard.fxml"));        
+                    Pane root = loader.load(getClass().getResource("AjouterClient.fxml"));
+                    Scene scene = new Scene(root);
+                    primaryStage.setTitle("Ajouter Client");
+                    primaryStage.setScene(scene);
+                    primaryStage.setResizable(false);
+                    primaryStage.show();
+        
+    }
 }
--- src/ModifierClientController.java
+++ src/ModifierClientController.java
@@ -4,9 +4,21 @@
  * and open the template in the editor.
  */
 
+import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javax.swing.JOptionPane;
+import project.ConnectionProvider;
 
 /**
  * FXML Controller class
@@ -18,9 +30,118 @@ public class ModifierClientController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
+        @FXML
+    private JFXTextField numClientField;
+    @FXML
+    private JFXTextField raisonScocialField;
+    @FXML
+    private JFXTextField adresseField;
+    @FXML
+    private JFXTextField matriculeFiscalField;
+    @FXML
+    private JFXTextField n_articleField;
+    @FXML
+    private JFXTextField registrDeCommrcField;
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
     
+    
+        @FXML
+    private void btnClose(){
+         //to close just one stage and other still running
+         Stage stage = (Stage) raisonScocialField.getScene().getWindow();
+         stage.close();    
+        // Platform.exit();
+    }
+    
+        @FXML
+    private void btnReinitialiser() throws IOException{
+        
+                    Stage stage = (Stage) raisonScocialField.getScene().getWindow();
+                    stage.close();  
+                    
+                    //Creat a new Satge to Show the New frame "the Deshboard"
+                    Stage primaryStage =new Stage();
+                    FXMLLoader loader =new FXMLLoader();
+                    //Parent root = loader.load(getClass().getResource("Deshboard.fxml"));        
+                    Pane root = loader.load(getClass().getResource("ModifierClient.fxml"));
+                    Scene scene = new Scene(root);
+                    primaryStage.setTitle("Modifier Client");
+                    primaryStage.setScene(scene);
+                    primaryStage.setResizable(false);
+                    primaryStage.show();
+        
+    }
+    
+        @FXML
+    private void btnRechercher(){
+                 if(numClientField.getText().isEmpty()){
+                    Toast.makeText((Stage) raisonScocialField.getScene().getWindow(), "Veuilley Entrer un N° Client SVP", 1500, 500, 500);
+                    return;
+                }
+        String clientId = numClientField.getText();
+        try {
+            Connection con= ConnectionProvider.getCon();
+            Statement st = con.createStatement();
+            ResultSet rs=st.executeQuery("select *from client where numeroClient='"+clientId+"'");
+            if(rs.next()){
+                raisonScocialField.setText(rs.getString(2));
+                adresseField.setText(rs.getString(3));
+                matriculeFiscalField.setText(rs.getString(4));
+                n_articleField.setText(rs.getString(5));
+                registrDeCommrcField.setText(rs.getString(6));
+                
+                numClientField.setEditable(false);
+            }else{
+                Toast.makeText((Stage) raisonScocialField.getScene().getWindow(), "Client n'existe pas", 1500, 500, 500);
+            }
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,""+e.toString());
+        }
+    }
+    
+            
+         @FXML
+    private void btnEnregistrer(){
+                 if(numClientField.getText().isEmpty()){
+                    Toast.makeText((Stage) raisonScocialField.getScene().getWindow(), "Veuilley Entrer un N° Client SVP", 1500, 500, 500);
+                    return;
+                }
+                String idClinet = numClientField.getText();
+                String raisonScociale = raisonScocialField.getText();
+                String adresse = adresseField.getText();
+                String matriculeFiscal = matriculeFiscalField.getText();
+                String nArticle = n_articleField.getText();
+                String registreDeCommerce = registrDeCommrcField.getText();
+
+                try {
+                    Connection con = ConnectionProvider.getCon();
+                    Statement st = con.createStatement();
+                    st.executeUpdate("update client set raisonSociale='"+raisonScociale+"',adresse='"+adresse+"',matriculeFiscale='"+matriculeFiscal+"',nArticle='"+nArticle+"',registreDeCommerce='"+registreDeCommerce+"' where numeroClient='"+idClinet+"'");
+                    
+                    Stage stage = (Stage) raisonScocialField.getScene().getWindow();
+                    stage.close();  
+                    
+                    //Creat a new Satge to Show the New frame "the Deshboard"
+                    Stage primaryStage =new Stage();
+                    FXMLLoader loader =new FXMLLoader();
+                    //Parent root = loader.load(getClass().getResource("Deshboard.fxml"));        
+                    Pane root = loader.load(getClass().getResource("ModifierClient.fxml"));
+                    Scene scene = new Scene(root);
+                    primaryStage.setTitle("Modifier Client");
+                    primaryStage.setScene(scene);
+                    primaryStage.setResizable(false);
+                    primaryStage.show();
+
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null,""+e.toString());
+                }
+        
+    }
 }
