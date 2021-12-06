40938279230ce9ed002db1af5ec75c155a433f84
issam <issam@DESKTOP-BI9SMQP>
2021-12-06 20:14:22 +0100

    list button action

--- src/DeshboardController.java
+++ src/DeshboardController.java
@@ -4,23 +4,133 @@
  * and open the template in the editor.
  */
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author abdel
- */
+
+
 public class DeshboardController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        
+    }  
+    @FXML
+    private void bntAjouterC() throws IOException{
+     
+                        Stage primaryStage =new Stage();
+                        FXMLLoader loader =new FXMLLoader();
+                        Parent root = loader.load(getClass().getResource("AjouterClient.fxml"));        
+                    
+                        Scene scene = new Scene(root);
+                        primaryStage.setTitle("Se Connecter");
+                        primaryStage.setScene(scene);
+                    
+                        primaryStage.show();
+    }
+    @FXML
+     private void bntlistc() throws IOException{
+     
+                        Stage primaryStage =new Stage();
+                        FXMLLoader loader =new FXMLLoader();
+                        Parent root = loader.load(getClass().getResource("ListDuClients.fxml"));        
+                    
+                        Scene scene = new Scene(root);
+                        primaryStage.setTitle("Se Connecter");
+                        primaryStage.setScene(scene);
+                    
+                        primaryStage.show();
+    }
+     @FXML
+     private void bntmodc() throws IOException{
+     
+                        Stage primaryStage =new Stage();
+                        FXMLLoader loader =new FXMLLoader();
+                        Parent root = loader.load(getClass().getResource("ModifierClient.fxml"));        
+                    
+                        Scene scene = new Scene(root);
+                        primaryStage.setTitle("Se Connecter");
+                        primaryStage.setScene(scene);
+                    
+                        primaryStage.show();
+    }
+     @FXML
+     private void bntsupc() throws IOException{
+     
+                        Stage primaryStage =new Stage();
+                        FXMLLoader loader =new FXMLLoader();
+                        Parent root = loader.load(getClass().getResource("SupprimerClient.fxml"));        
+                    
+                        Scene scene = new Scene(root);
+                        primaryStage.setTitle("Se Connecter");
+                        primaryStage.setScene(scene);
+                    
+                        primaryStage.show();
+    }
+     @FXML
+     private void bntlistp() throws IOException{
+     
+                        Stage primaryStage =new Stage();
+                        FXMLLoader loader =new FXMLLoader();
+                        Parent root = loader.load(getClass().getResource("ListeDuProduits.fxml"));        
+                    
+                        Scene scene = new Scene(root);
+                        primaryStage.setTitle("Se Connecter");
+                        primaryStage.setScene(scene);
+                    
+                        primaryStage.show();
+    }
+     @FXML
+     private void bntajoutp() throws IOException{
+     
+                        Stage primaryStage =new Stage();
+                        FXMLLoader loader =new FXMLLoader();
+                        Parent root = loader.load(getClass().getResource("AjouterProduit.fxml"));        
+                    
+                        Scene scene = new Scene(root);
+                        primaryStage.setTitle("Se Connecter");
+                        primaryStage.setScene(scene);
+                    
+                        primaryStage.show();
+    }
+     @FXML
+     private void bntmodp() throws IOException{
+     
+                        Stage primaryStage =new Stage();
+                        FXMLLoader loader =new FXMLLoader();
+                        Parent root = loader.load(getClass().getResource("ModifierProduit.fxml"));        
+                    
+                        Scene scene = new Scene(root);
+                        primaryStage.setTitle("Se Connecter");
+                        primaryStage.setScene(scene);
+                    
+                        primaryStage.show();
+    }
+     @FXML
+     private void bntsupp() throws IOException{
+     
+                        Stage primaryStage =new Stage();
+                        FXMLLoader loader =new FXMLLoader();
+                        Parent root = loader.load(getClass().getResource("SupprimerProduit.fxml"));        
+                    
+                        Scene scene = new Scene(root);
+                        primaryStage.setTitle("Se Connecter");
+                        primaryStage.setScene(scene);
+                    
+                        primaryStage.show();
+    }
     
+    
+     
 }
--- src/project/ConnectionProvider.java
+++ src/project/ConnectionProvider.java
@@ -1,16 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
+
+
 package project;
 import java.sql.*;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author abdel
- */
+
 public class ConnectionProvider {
     public static Connection getCon(){
         try {
