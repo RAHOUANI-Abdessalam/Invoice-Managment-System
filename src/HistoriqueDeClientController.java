941375a8d1796b19afaecd07934df9cd7996cac0
RAHOUANI-Abdessalam <abdelsalamrahouani@gmail.com>
2021-12-07 19:27:47 +0100

    add jar libs

--- src/HistoriqueDeClientController.java
+++ src/HistoriqueDeClientController.java
@@ -6,7 +6,10 @@
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,12 +18,21 @@ import javafx.fxml.Initializable;
  */
 public class HistoriqueDeClientController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+
+    @FXML
+    private ImageView close;
+        
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    @FXML
+    private void btnClose(){
+         //to close just one stage and other still running
+         Stage stage = (Stage) close.getScene().getWindow();
+         stage.close();    
+        // Platform.exit();
+    }
     
 }
